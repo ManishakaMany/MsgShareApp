4dc8de469bf018f473371ca1476e8a6eac5df965
ManishakaMany <mm67233@gmail.com>
2020-05-17 00:25:24 +0530

    Section Two, Video Three

--- app/src/main/java/com/indi/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/indi/msgshareapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.indi.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,5 +17,12 @@ class MainActivity : AppCompatActivity() {
             Log.i("MainActivity","Button Was Clicked !")
         Toast.makeText(this,"Button was Clicked !",Toast.LENGTH_SHORT).show()
         }
+btnSendMsgToNextActivity.setOnClickListener {
+    Log.i("MainActivity","Second Button Was Clicked !")
+val message: String = etUserMessage.text.toString()
+    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+val intent = Intent(this,SecondActivity::class.java)
+    startActivity(intent)
+}
     }
 }
