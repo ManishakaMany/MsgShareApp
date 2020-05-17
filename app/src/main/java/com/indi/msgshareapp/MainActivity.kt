a79769dec17e1f1f2fe0dc0eb3de0d66622acc8b
ManishakaMany <mm67233@gmail.com>
2020-05-17 21:22:31 +0530

    Section Two, Video Five

--- app/src/main/java/com/indi/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/indi/msgshareapp/MainActivity.kt
@@ -25,5 +25,14 @@ val intent = Intent(this,SecondActivity::class.java)
     intent.putExtra("user_message",message)
     startActivity(intent)
 }
+        btnShareToOtherApps.setOnClickListener{
+            val message: String = etUserMessage.text.toString()
+
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent,"Share with"))
+        }
     }
 }
